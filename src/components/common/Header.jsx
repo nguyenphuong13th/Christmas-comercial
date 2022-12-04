7692631dd137d30cfbb859af3b96e4d0f89b5fbf
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-12-04 23:23:20 +0700

    finish cart function add,delete

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect  } from 'react'
 import logo from '../assets/images/logo.png'
+import cart from '../assets/images/cart/cart.png'
 import { BsBagCheck } from 'react-icons/bs';
 import { BiSearch } from 'react-icons/bi';
 import { RiUser3Line } from 'react-icons/ri';
-import { AiOutlineMenu,AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineMenu,AiOutlineClose, AiOutlineDelete } from 'react-icons/ai';
 import { navlist } from '../assets/data/data';
 import { Link } from 'react-router-dom';
 import { connect,useDispatch,useSelector } from 'react-redux';
 import { cartlistSelector } from '../../controller/cartListSelector';
+import { removeCart } from 'controller/cartListReducerSlice'
 
 export const Header = () => {
 
@@ -16,11 +18,56 @@ export const Header = () => {
         header.classList.toggle("active",this.window.scrollY > 100) //if croll down >100 add class active cho header
     })
     const [mobile,setMobile] = useState(false) //toggle menu responsive in mobile mode
-
+    // useDispatch
+    const dispatch = useDispatch();
     //cart add in shop
     const getdata = useSelector(cartlistSelector)
-    console.log(getdata)
-    console.log("cartlistSelector"+ cartlistSelector)
+
+    //cart submenu
+    const [cartList,setCartList] = useState(false)
+    // handle onclick on cart logo
+    const handleClose = ()=>{
+      setCartList(null)
+    }
+    // total price state
+    const [totalPrice,setTotalPrice] = useState(0)
+    //use data of items in cart to calculate total price
+    const totalPriceCart = () =>{
+      let totalPrice = 0
+      getdata.cart.map((p,i)=>{
+        totalPrice = parseFloat(p.price) * p.qty + totalPrice
+      })
+      setTotalPrice(totalPrice)
+    }
+    // update state when total update totalPriceCart reload page to show state changed
+    useEffect(()=>{
+      totalPriceCart()
+    },[totalPriceCart])
+
+    //show data from getdata tranfer to cart
+    const showCartList = getdata.cart.map((showCartList)=>(
+      <div className="details_content">
+        <div className="details_content_img">
+          <Link to={`/cart/${showCartList.id}`} onClick={handleClose}>
+            <img src={require("components/assets"+showCartList.cover)} alt="" />
+          </Link>
+        </div>
+        <div className="details_content_detail">
+          <div className="details_content_detail_price">
+            <p>{showCartList.title.slice(0,20)} ...</p>
+            <p>Price:${showCartList.price}</p>
+            <p>Quantity: {showCartList.qty}</p>
+          </div>
+        </div>
+        {/* delete or remove item from cart */}
+        <div className="details_content_detail_icon">
+          <i onClick={()=> dispatch(removeCart(showCartList.id))}>
+            <AiOutlineDelete/>
+          </i>
+        </div>
+      </div>
+    ))
+
   return (
     <>
       <header className="header">
@@ -67,10 +114,29 @@ export const Header = () => {
               </span>
             </div>
             <div className="right_card">
-              <button className="button">
+              <button className="button" onClick={()=>setCartList(!cartList)}>
                 <BsBagCheck className="shop heIcon" />
                 MY CART ({getdata.cart? getdata.cart.length:0})
               </button>
+              <div className={cartList? "showCart" :"hideCart"}>
+                  {getdata.cart.length ? (
+                    <section className="details">
+                      <div className="details_title">
+                        <h3>Photo</h3>
+                        <p>Product Name</p>
+                      </div>
+                      {showCartList}
+                      <div className="details_total">
+                        <h4>Total : ${totalPrice}</h4>
+                      </div>
+                    </section>
+                  ):(
+                    <div className="empty">
+                      <p>Your cart is Empty</p>
+                      <img src={cart} alt="" />
+                    </div>
+                  )}
+              </div>
             </div>
           </div>
         </div>
--- src/controller/cartListReducerSlice.js
+++ src/controller/cartListReducerSlice.js
@@ -4,13 +4,32 @@ const initialState = {
     cart:[]
 }
 const cartlistReducerSlice = createSlice({
-    name:"addCartList",
+    name:"CartList",
     initialState,
     reducers:{
         addCart:(state,action)=>{
-            state.cart.push(action.payload)
+            // Find the element in cart which have the same id as the selected item return the first index of the match item in cart.
+            const itemIndex = state.cart.findIndex((itemIndex)=> itemIndex.id === action.payload.id)
+
+            // if have 1 selected item the quantity of the item in cart will increase by 1
+            if(itemIndex >= 0 ){
+                state.cart[itemIndex].qty += 1
+            }else{
+                // In case cannot find an similar item in cart add the selected item as the new item with quantity is 1
+                const temp = {...action.payload,qty : 1}
+                state.cart.push(temp)
+            }
+
+        },
+        removeCart :(state,action) =>{
+            //ID of an item have put in cart dispatched by an onClick on delete symbol
+            const itemID = action.payload
+            // if dispatched ID match with ID of item in cart then we do not show that item
+            state.cart = state.cart.filter((element)=> element.id !== itemID)
+
         }
+
     }
 })
-export const {addCart} = cartlistReducerSlice.actions
+export const {addCart,removeCart} = cartlistReducerSlice.actions
 export default cartlistReducerSlice.reducer;
