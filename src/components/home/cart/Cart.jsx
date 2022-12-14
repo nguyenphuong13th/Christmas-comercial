99d9027659446351d2f6bcab341d5ebcc95349ca
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-12-14 17:53:53 +0700

    adding cart check out function

--- src/components/home/cart/Cart.jsx
+++ src/components/home/cart/Cart.jsx
@@ -11,7 +11,23 @@ export const Cart  = () => {
   const getdata = useSelector(cartlistSelector)
   const dispatch = useDispatch()
   const {id} = useParams()
-  console.log(getdata)
+  const [totalPrice,setTotalPrice] = useState(0)
+  const totalPriceCart = () =>{
+    let totalPrice = 0
+    getdata.cart.map((p,i)=>{
+      totalPrice = parseFloat(p.price) * p.qty + totalPrice
+    })
+    setTotalPrice(totalPrice)
+  }
+  // update state when total update totalPriceCart reload page to show state changed
+  useEffect(()=>{
+    totalPriceCart()
+  },[totalPriceCart])
+  const deductVoucher = 0
+  const shippingPrice = 25
+  const taxPrice = totalPrice ? (totalPrice * 10) / 100 : 0
+  const finalPay = totalPrice + taxPrice + shippingPrice - deductVoucher
+
   // const increment = (cartdataCheckoutList)=>{
   //   dispatch(addCart(cartdataCheckoutList))
   // }
@@ -27,35 +43,48 @@ export const Cart  = () => {
 
 
 
-  const cartdataCheckoutList = getdata.cart ? getdata.cart.map((cartdataCheckoutList) => (
-    <div className="cart_details_content">
-      <div className="cart_details_content_img">
-        <img
-          src={require("components/assets" + cartdataCheckoutList.cover)}
-          alt=""
-        />
-        <p>{cartdataCheckoutList.title}</p>
-      </div>
-      <div className="detail_content_details">
-        <p>{cartdataCheckoutList.price}</p>
-      </div>
-      <div className="qty">
-        <div className="count">
-          <button >
-            <AiOutlinePlus />
-          </button>
-          {/* onClick={() => increment(cartdataCheckoutList)} */}
-          <span>
-            <input type="text" value={cartdataCheckoutList.qty} />
-          </span>
-          <button >
-            <AiOutlineMinus />
-          </button>
-          {/* onClick={() => decrement(cartdataCheckoutList)} */}
-        </div>
-      </div>
-    </div>
-  ) ): <p>Nothing to show</p>;
+  const cartdataCheckoutList = getdata.cart ? (
+    getdata.cart.map((cartdataCheckoutList) => (
+      <tr className="detail_content_details_products">
+          <td>
+            <div className="cart_details_content_img">
+              <img
+                src={require("components/assets" + cartdataCheckoutList.cover)}
+                alt=""
+              />
+              <p>{cartdataCheckoutList.title}</p>
+            </div>
+          </td>
+          <td>
+            <div className="detail_content_details_price">
+              <p>{cartdataCheckoutList.price}$</p>
+            </div>
+          </td>
+          <td>
+            <div className="detail_content_details_qty">
+              <button>
+                <AiOutlinePlus />
+              </button>
+              {/* onClick={() => increment(cartdataCheckoutList)} */}
+              <span>
+                <input type="text" value={cartdataCheckoutList.qty} />
+              </span>
+              <button>
+                <AiOutlineMinus />
+              </button>
+              {/* onClick={() => decrement(cartdataCheckoutList)} */}
+            </div>
+          </td>
+          <td>
+            <div className="detail_content_details_total">
+              <p>{cartdataCheckoutList.price}$</p>
+            </div>
+          </td>
+      </tr>
+    ))
+  ) : (
+    <h2>Your Cart is empty</h2>
+  );
 
   return (
     <>
@@ -63,23 +92,56 @@ export const Cart  = () => {
         <div className="cart_title">
           <h1>Shopping Cart</h1>
         </div>
-        <div className="cart_header">
-          <section className='cart_product'>
-            <p>Product</p>
-          </section>
-          <section className='cart_price'>
-            <p>Price</p>
-          </section>
-          <section className='cart_qty'>
-            <p>Qty</p>
-          </section>
-          <section className='cart_total'>
-            <p>Total</p>
-          </section>
+        <div className="cart_details_content">
+          <table className="cart_details_content_table">
+            <tr>
+              <th>
+                <div className="cart_product">
+                  <p>Product</p>
+                </div>
+              </th>
+              <th>
+                <div className="cart_price">
+                  <p>Price</p>
+                </div>
+              </th>
+              <th>
+                <div className="cart_qty">
+                  <p>Qty</p>
+                </div>
+              </th>
+              <th>
+                <div className="cart_total">
+                  <p>Total</p>
+                </div>
+              </th>
+            </tr>
+            {cartdataCheckoutList}
+          </table>
+            <table className="ckeck_out_content_table">
+              <tr className="total_products">
+                <td><h3>Total:</h3></td>
+                <td> <p>${totalPrice}</p></td>
+              </tr>
+              <tr className="tax_products">
+                <td><h3>Tax (10%)</h3></td>
+                <td> <p>${taxPrice}</p></td>
+              </tr>
+              <tr className="shipping_products">
+                <td><h3>Shipping</h3></td>
+                <td><p>$25</p></td>
+              </tr>
+              <tr className="Deduct_products">
+                <td><h3>Voucher</h3></td>
+                <td><p>${deductVoucher}</p></td>
+              </tr>
+              <tr className="checkout_price">
+                <td><h3>Final Pay : </h3></td>
+                <td><p>${finalPay}</p></td>
+              </tr>
+            </table>
+            <a className="button" href="/">Check out</a>
         </div>
-        {cartdataCheckoutList}
-        <br></br>
-
 
       </div>
     </>
